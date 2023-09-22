f83cc6038cacd6d403cbc565bed3644b5e129d2d
Notakachimoto <slavinskiy03@bk.ru>
2023-09-22 12:03:29 +0300

    second step done

--- src/solution.js
+++ src/solution.js
@@ -1,13 +1,26 @@
-export default (content) => {
-    // Первое Задание
-    const data = content.split('\n');
-    const rows = data.slice(1).map((row) => row.split('|'));
-    console.log(`Count: ${rows.length}`);
-
-    // Второе Задание
-    const strength = rows.map((row) => row[2]);
-
-    // const strongest = Number(strength)
-    console.log(strength.join(''));
+export default (content) => { 
+    const data = content.split('\n').slice(1); 
+    console.log(`Количество рядов существ: ${data.length}`) 
+ 
+    const rows = data.map((row) => row 
+    .split('|') 
+    .filter((row) => row)) 
+    .map((array) => array  
+        .map((element) => element.trim()) 
+        ); 
+ 
+    const creatures = rows.map((row) => row[0]); 
+    const price = rows.map((row) => Number(row[6])); 
+ 
+ 
+    const strengths = rows.map((row) => Number(row[1])); 
+    const strongestIndex = strengths.indexOf(Math.max(...strengths)); 
+    const strengthsWithoutStrongest = strengths.slice(); 
+    strengthsWithoutStrongest[strongestIndex] = 0; 
+    const secondStrongestIndex = strengthsWithoutStrongest.indexOf(Math.max(...strengthsWithoutStrongest)); 
+     
+    console.log(`Цена за 10 сильнейших созданий: ${price[strongestIndex]*10}`); 
+    console.log(`Цена за 20 вторых по силе созданий: ${price[secondStrongestIndex]*20}`); 
+ 
 
 }
